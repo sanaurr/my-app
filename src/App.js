b5b8465d4c5fd25a23899af370591c97d14f9f63
Sanaur <sanaur.sp@gmail.com>
2023-05-20 17:36:21 +0600

    kfhgjkfh

--- src/App.js
+++ src/App.js
@@ -2,17 +2,64 @@ import './App.css';
 import { useState } from 'react';
 import React from 'react';
 import myImage from './demo-img/download.jpg';
+import { styled } from 'styled-components';
+
+// Styled component for the card container
+const CardContainer = styled.div`
+  width: 300px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 20px;
+`;
+
+// Styled component for the image
+const CardImage = styled.img`
+  width: 100%;
+  height: auto;
+  border-radius: 4px;
+`;
+
+// Styled component for the title
+const CardTitle = styled.h3`
+  font-size: 18px;
+  margin-top: 10px;
+`;
+
+// Styled component for the price
+const CardPrice = styled.p`
+  font-size: 16px;
+  margin-top: 5px;
+`;
+
+// Styled component for the button
+const CardButton = styled.button`
+  padding: 8px 12px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  margin-top: 10px;
+`;
+
+const Card = ({ imageSrc, title, price, buttonText }) => {
+  return (
+    <CardContainer>
+      <CardImage src={imageSrc} alt={title} />
+      <CardTitle>{title}</CardTitle>
+      <CardPrice>{price}</CardPrice>
+      <CardButton>{buttonText}</CardButton>
+    </CardContainer>
+  );
+};
 
 
 const HomePage = () => {
   return (
     <>
       <h1>Our products </h1>
-      <div>
-        <img src={myImage} alt="KTM" /> 
-      </div>
-        
-      
+      <Card imageSrc={myImage} title={'Motorsikal'} price={'1200RM'} buttonText={'Buy now'}/>
+      <Card imageSrc={myImage} title={'Motorsikal'} price={'1200RM'} buttonText={'Buy now'}/>
     </>
   );
 }
